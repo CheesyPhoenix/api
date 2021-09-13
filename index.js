ee0f9cb021488433e83c29f47ce596a13e05941d
CheesyPhoenix <sindre.he.moe@gmail.com>
2021-09-13 10:40:49 +0200

    SM update

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+//docker run: docker run -d --env DATA_PATH=/src/data/MenuSites.json --mount type=volume,src=testVolume,target=/src/data -p 8080:8080 cheesyphoenix/websiteapi:1.1
+
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const path = require("path");
+
+const dataPath = path.join(process.env.DATA_PATH || "./data/Menusites.json");
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/tshirt", (req, res) => {
-	res.status(200).send(JSON.parse(fs.readFileSync("MenuSites.json")));
+	res.status(200).send(JSON.parse(fs.readFileSync(dataPath)));
 });
 app.get("/document/:path", (req, res) => {
 	const { path } = req.params;
 	res.status(200).sendFile(path, { root: __dirname });
 });
 app.post("/tshirt", (req, res) => {
-	fs.writeFileSync("MenuSites.json", JSON.stringify(req.body, null, 2));
+	fs.writeFileSync(dataPath, JSON.stringify(req.body, null, 2));
 	res.status(201);
 });
 
